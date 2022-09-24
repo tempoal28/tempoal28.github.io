6b92d5476d067ff1f00d1b4b6f2410c0c08214a4
tempoal28 <114294398+tempoal28@users.noreply.github.com>
2022-09-24 20:21:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var data = new Date("Jul 28 2032, 00:00:00").getTime()
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = data - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24 ))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+document.getElementById('days').innerHTML = days;
+document.getElementById('hours').innerHTML = hours;
+document.getElementById('minutes').innerHTML = minutes;
+document.getElementById('seconds').innerHTML = seconds;
+
+},1000); 
