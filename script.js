fbf46c9c934536469ff8373276b6e1df702f3235
tempoal28 <114294398+tempoal28@users.noreply.github.com>
2022-09-25 13:04:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var data = new Date("Jul 28 2032, 00:00:00").getTime()
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = data - now;
+    console.log(distance)
     var days = Math.floor(distance / (1000 * 60 * 60 * 24 ))
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60))
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -11,5 +12,14 @@ document.getElementById('days').innerHTML = days;
 document.getElementById('hours').innerHTML = hours;
 document.getElementById('minutes').innerHTML = minutes;
 document.getElementById('seconds').innerHTML = seconds;
+
+if (days == 3591) {
+    document.getElementById('auguri').innerHTML = "Buon 28 settembre";
+}
+
+if (days < 3591) {
+    document.getElementById('auguri').innerHTML = "";
+
+}
 
 },1000); 
