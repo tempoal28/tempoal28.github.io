1bcc395a01c7e3f5b8eeaf9dd27da15cca1ddd2b
tempoal28 <114294398+tempoal28@users.noreply.github.com>
2022-09-28 22:48:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ if (days == 2859) {
     document.getElementById('auguri').innerHTML = "Buon 28 settembre +1.2 <3";
     document.getElementById('ciao').style.color = "blue";
     document.getElementById('auguri').style.textShadow = "0px 0px 22px blue"
-
 }
 
+
+//animazione finale:
 if (days < 2859) {
     document.getElementById('auguri').innerHTML = "";    
     document.getElementById('title').innerHTML = "<3"
@@ -26,11 +27,27 @@ if (days < 2859) {
     document.getElementById('auguri').style.textShadow = ""
 }
 
+if (days == 2859 && hours <= 00 && minutes <= 00 && seconds < 10) {
+    document.getElementById('span').style.animation = "span 6s"
+    document.getElementById('days').style.animation = "division 6s";
+    document.getElementById('seconds').style.animation = "division 6s";
+    document.getElementById('hours').style.animation = "division-mid 6s";
+    document.getElementById('seconds').style.animation = "division-mid 6s";
+}
+
+
+if (days == 2859 && hours == 00 && minutes == 00 && seconds <= 5) {
+    document.getElementById('span').style.animationPlayState = "paused"
+    document.getElementById('days').style.animationPlayState = "paused"
+    document.getElementById('hours').style.animationPlayState = "paused"
+    document.getElementById('minutes').style.animationPlayState = "paused"
+    document.getElementById('seconds').style.animationPlayState = "paused"
+}
 
 
 },1000); 
 
-var date = new Date("Sep 28 2022, 15:42:15").getTime()
+var date = new Date("Sep 28 2022, 00:00:00").getTime()
 var a = setInterval(function() {
     var ora = new Date().getTime();
     var distances = date - ora;
@@ -45,7 +62,7 @@ document.getElementById('minutes1').innerHTML = minute;
 document.getElementById('seconds1').innerHTML = second;
 
 if (day <= 00 && hour <= 00 && minute <= 00 && second <= 00) {
-    var doc = document.getElementById('span1');
+    var doc = document.getElementById('spann');
     while (doc.hasChildNodes()) {
     doc.removeChild(doc.firstChild)
 }
